4522d69a8f7e0734073fda48acb000b6139118b8
MFJcode <mfjcode@gmail.com>
2021-09-11 21:08:50 +0200

    First Commit

---
+++ assets/js/index.js
@@ -0,0 +1,65 @@
+
+
+//document.getElementById("count-el").innerText = 5
+
+// let firstBatch = 5;
+// let secondBach = 7;
+// let count = firstBatch + secondBach;
+// console.log(count);
+
+// 1. Create two variables, myAge and humanDogRatio
+// 2. Multiply the two together and store the result in myDogAge 
+// 3. Log myDogAge to the console
+
+// myAge = 50;
+// humanDogRatio = 7;
+
+// myDogAge = myAge * humanDogRatio;
+
+// console.log('My dog age is ' + myDogAge);
+
+// -------------------------------
+
+// let count = 5
+
+// // count + 1
+// count = count + 1 // 5 + 1 = 6
+
+// console.log(count)
+
+//-------------------------------------
+
+// let count = 5
+
+// // count + 1
+// count = count + 2
+
+
+// count = count + 10
+
+// count = count - 2
+
+// console.log(count)
+
+// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
+// Decrease it down to 25, and then finally increase it to 70
+// Console.log the value after each step
+
+let bonusPoints = 50;
+
+console.log(bonusPoints);
+
+bonusPoints = bonusPoints + 100;
+
+console.log(bonusPoints);
+
+bonusPoints = bonusPoints - 25;
+
+console.log(bonusPoints);
+
+bonusPoints = bonusPoints + 70;
+
+console.log(bonusPoints);
+
+
+
