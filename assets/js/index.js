8a59e99c93c00f2eeffe48dc66277ece0dd385f4
MFJcode <mfjcode@gmail.com>
2021-09-12 20:39:23 +0200

    Create a SAVE button and Create a function save()

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,6 +9,12 @@ function increment() {
     countEl.innerText = count;
 }
 
+// 1. Create a function, save(), which logs out the count when it's called
+
+function save() {
+    console.log(count);
+}
+
 
 
 
