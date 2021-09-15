d54bff1b723be82cfb758ce1e3067ad24b63edc2
MFJcode <mfjcode@gmail.com>
2021-09-15 07:40:41 +0200

    Set the count to 0

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,9 +16,11 @@ function save() {
     let countStr = count + " - "
     // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
     // 3. Render the variable in the saveEl using innerText
-    saveEl.textContent += countStr
     // NB: Make sure to not delete the existing content of the paragraph
-    console.log(count)
+    saveEl.textContent += countStr
+
+    countEl.textContent = 0
+    count = 0
 }
 
 
